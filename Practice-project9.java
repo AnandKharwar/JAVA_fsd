d5ed9139378e605f5cf4d13b0c6288de41a316bc
AnandKharwar <anandkharwar2000@gmail.com>
2022-07-19 22:44:01 +0530

    Practice

---
+++ Practice-project9.java
@@ -0,0 +1,17 @@
+package practice.array;
+
+public class arrayAssisted {
+
+	public static void main(String[] args) {
+		
+		int a[]={10,20,30,40,50};
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("Element of array a:"+a[i]);
+			
+		}
+		int [][]b= {{2,4,6,8},{3,6,9}};
+		System.out.println("\nlength of row1:"+b[0].length);
+		
+				}
+	}
