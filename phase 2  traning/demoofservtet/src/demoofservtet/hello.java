03a2c9180b4de3ab93411894a62560d74172e8af
AnandKharwar <anandkharwar2000@gmail.com>
2022-08-09 15:52:48 +0530

    phase2

---
+++ phase 2  traning/demoofservtet/src/demoofservtet/hello.java
@@ -0,0 +1,27 @@
+package demoofservtet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class hello extends  HttpServlet   {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException , IOException
+	{
+		
+		PrintWriter out= resp.getWriter();
+		out.print("Welcome"); //write out put in browser
+		
+		
+	}
+
+}
+
